685a1e24b4ceaafaa4b6d6ee4fed9f601c4789f2
AshtonJ7 <AshtonJ7@users.noreply.github.com>
2023-09-10 18:30:35 +0100

    resume added

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -28,10 +28,11 @@ function ContactForm() {
       <label htmlFor="name"> Name </label>
       <input type="text" id="fname" name="firstname" placeholder="Your name" required></input>
       <label htmlFor="email"> Email Address </label>
-      <input id="email" type="email"  name="email"  />  <ValidationError 
+      <input id="email" type="email"  name="email"  required> 
+      </input>  <ValidationError 
         prefix="Email"  field="email" errors={state.errors}  />
        <label htmlFor="message"> Message</label>
-      <textarea id="message"  name="message" />
+      <textarea id="message"  name="message" required/>
       <ValidationError   prefix="Message"  field="message" errors={state.errors}
       />
       <button type="submit" disabled={state.submitting}>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,6 +9,15 @@ import jate from '../Assets/JATE.png';
 import msa from '../Assets/MSA.png';
 import tech from '../Assets/tech-corner.png';
 
+import Button from "react-bootstrap/Button";
+import pdf from "../Assets/Resume.pdf"
+import { AiOutlineDownload } from "react-icons/ai";
+import { pdfjs } from "react-pdf";
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`
+
+
+
 function Projects() {
   return (
     <Container fluid className="project-section" id="projects">
@@ -42,7 +51,6 @@ function Projects() {
               demoLink="https://elleinadseer.github.io/speak-my-words/"
             />
           </Col>
-
           
           <Col md={4} className="project-card">
             <ProjectCard
@@ -91,9 +99,19 @@ function Projects() {
               demoLink="https://makenotes-03c8c8e35784.herokuapp.com/"              
             />
           </Col>
-
         </Row>
       </Container>
+      <div className= "resume">
+      <Button 
+            variant="primary"
+            href={pdf}
+            target="_blank"
+            style={{ maxWidth: "250px"}}
+          >
+            <AiOutlineDownload />
+            &nbsp;View/Downloard Resume
+          </Button>
+      </div>
     </Container>
   );
 }
