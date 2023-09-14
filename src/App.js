db0862927675532b8b1db60b7e801fa39fc3f609
AshtonJ7 <AshtonJ7@users.noreply.github.com>
2023-09-14 13:58:12 +0100

    style sheet added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Resume from "./components/Resume";
+import './styles/style.css';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
